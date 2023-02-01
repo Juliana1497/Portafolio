151ec7c274a46b1255cb575286f586e4a27e4fb2
Juliana1497 <cristanchojuli@gmail.com>
2023-02-01 10:07:18 -0500

    arreglando nav

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,30 +1,40 @@
-import React, {useState} from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
-import Modal from '../peyes/Modal';
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import Modal from "../peyes/Modal";
 
 const Header = () => {
-  const [stateModal,changeState] = useState(false);
+  const [stateModal, changeState] = useState(false);
+
+  //change nav color when scrolling
+  
+
   return (
-    <div>
-    <div className='containerHeader'>
-      <div className='nav'>
-          <Link to='/'>
+    <div >
+      <div className="containerHeader">
+        <div className="nav">
+          <Link to="/">
             <p>Home</p>
           </Link>
-          <a href="#knowledge"><p>Knowledge</p></a> 
-          <a href="#projects"><p>Projects</p></a> 
-          <Link to='/curriculum'>
+          <a href="#knowledge">
+            <p>Knowledge</p>
+          </a>
+          <a href="#projects">
+            <p>Projects</p>
+          </a>
+          <Link to="/curriculum">
             <p>Curriculum</p>
           </Link>
+        </div>
+        <div className="contact">
+          <p className="contactP" onClick={() => changeState(!stateModal)}>
+            -CONTAC ME-
+          </p>
+        </div>
+        <Modal state={stateModal} changeState={changeState} />
       </div>
-      <div className='contact'>
-        <p className='contactP' onClick={() => changeState(!stateModal)}>-CONTAC ME-</p>
-      </div>
-      <Modal state={stateModal} changeState={changeState}/>
-  </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
